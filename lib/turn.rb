b02986cb6abed2e4220afc16c5c872ece404a09e
bcarrol2 <blairacarroll@gmail.com>
2018-09-30 16:27:04 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,23 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(moves)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+display_board(board)
+
+def valid_move?(validity)
+  
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+end
+
+def moves
+  
+end
